f61e7a86b339936f91210df85787ba419e33f3b6
Kata Durjyodhan Reddy <kdurjyodhan141@gmail.com>
2023-10-26 21:43:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,35 @@
 function threeSum(arr, target) {
 // write your code here
+	 if (arr.length < 3) {
+    // Not enough elements to form a sum of three integers.
+    return null;
+  }
+
+  // Sort the array in ascending order.
+  arr.sort((a, b) => a - b);
+
+  let closestSum = arr[0] + arr[1] + arr[2];
+
+  for (let i = 0; i < arr.length - 2; i++) {
+    let left = i + 1;
+    let right = arr.length - 1;
+
+    while (left < right) {
+      const currentSum = arr[i] + arr[left] + arr[right];
+
+      if (Math.abs(currentSum - target) < Math.abs(closestSum - target)) {
+        closestSum = currentSum;
+      }
+
+      if (currentSum < target) {
+        left++;
+      } else {
+        right--;
+      }
+    }
+  }
+
+  return closestSum;
   
 }
 
